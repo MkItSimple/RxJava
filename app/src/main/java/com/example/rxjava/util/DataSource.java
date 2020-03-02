a8495e91cc15bc34d9ff7ca7e74c41baf3f2e1e7
MkItSimple <choanania1@gmail.com>
2020-03-03 04:46:46 +0800

    Branch01

--- app/src/main/java/com/example/rxjava/util/DataSource.java
+++ app/src/main/java/com/example/rxjava/util/DataSource.java
@@ -9,5 +9,11 @@ public class DataSource {
 
     public static List<Task> createTaskList(){
         List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task("Take our the trash", true, 3));
+        tasks.add(new Task("Walk the dog", true, 2));
+        tasks.add(new Task("Make my bed", true, 1));
+        tasks.add(new Task("Unload the dishwasher", true, 0));
+        tasks.add(new Task("Make a dinner", true, 5));
+        return tasks;
     }
 }
